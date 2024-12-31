80c50bd7c5725181108f5ceb3fa0b8a573a5c345
Ross Lehr <itsme@rosslehr.com>
2024-12-31 11:44:47 -0500

    Finished contact info component

--- src/app/components/display-contact-info/display-contact-info.component.ts
+++ src/app/components/display-contact-info/display-contact-info.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ConInfoPhysical {
+  name: string;
+  text: string;
+  icon: string;
+}
+
+interface ConInfoSocial {
+  name: string;
+  link: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-display-contact-info',
   standalone: true,
   imports: [],
   templateUrl: './display-contact-info.component.html',
-  styleUrl: './display-contact-info.component.css'
+  styleUrls: ['./display-contact-info.component.css'], // Fixed typo
 })
-export class DisplayContactInfoComponent {
+export class DisplayContactInfoComponent implements OnInit {
 
+  @Input() socialOnly: boolean = false;
+
+  conInfoPhysical: ConInfoPhysical[] = [];
+  conInfoSocial: ConInfoSocial[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<{ physical: ConInfoPhysical[]; social: ConInfoSocial[] }>(
+      'assets/JSON/contact-information.json'
+    ).subscribe(
+      (response) => {
+        // Assign the fetched JSON data to respective properties
+        this.conInfoPhysical = response.physical;
+        this.conInfoSocial = response.social;
+      },
+      (error) => {
+        // Log an error message if the JSON file cannot be loaded
+        console.error('Error fetching JSON file:', error);
+      }
+    );
+  }
+
+  get getConInfoPhysical() {
+    return this.conInfoPhysical;
+  }
+
+  get getConInfoSocial() {
+    return this.conInfoSocial;
+  }
 }
