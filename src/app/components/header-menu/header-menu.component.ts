73206b88e84d18127bb37784e6620b1e78254348
rtlehr <itsme@rosslehr.com>
2025-01-24 11:03:31 -0500

    Clean up Header Menu

--- src/app/components/header-menu/header-menu.component.ts
+++ src/app/components/header-menu/header-menu.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { Location } from '@angular/common';
-import { ConfigService } from '../../services/config.service';
 import { RouterModule, Routes } from '@angular/router';
 import { Router } from '@angular/router';
 
@@ -15,64 +13,25 @@ import { Router } from '@angular/router';
 })
 export class HeaderMenuComponent {
 
-  @Input() menuFile: string = "";
-
   headerMenuItems: any[] = [];
-
-  currentUrl: string = '';
-
-  urlItems: string[] = [];
 
   config: any;
 
   menuItems: any[] = [];
 
-  constructor(private http: HttpClient, private location: Location, private configService: ConfigService, private router: Router) {} 
+  constructor(private http: HttpClient, private router: Router) {} 
 
   @Output() parentEvent = new EventEmitter<string>();
 
   ngOnInit(): void {
 
     const routes = this.router.config; 
+
     this.menuItems = this.extractMenuItems(routes);
-    console.log(' this.menuItems[1].children: ' + this.menuItems[1].children[0].title); 
-
-    /*
-    this.config = this.configService.getConfig();
-    
-    this.http.get<any[]>('assets/' + this.config.headerMenuPath).subscribe(
-      
-      (response) => {
-
-        this.headerMenuItems = response;
-
-        //let data: any = {'file':this.headerMenuItems[0].file, 'component': this.headerMenuItems[0].component};
-
-        this.currentUrl = this.location.path();
-
-        this.urlItems = this.currentUrl.slice(1).split("/");
-    
-        // Load the home page if no items in the URL
-        if (!this.urlItems[0]) {
-        
-          this.location.replaceState(this.headerMenuItems[0]?.url || "");
-              
-        }
-
-        this.parentEvent.emit();
-
-      },
-      (error) => {
-        console.error('Error fetching JSON file:', error); 
-      }
-      
-    );*/
 
   }
 
   private extractMenuItems(routes: any[]): any[] {
-
-    console.log('extractMenuItems: ' + routes[0].title);
 
     return routes
       .filter((route) => route.data?.menu) // Include only routes with `menu: true`
@@ -81,6 +40,7 @@ export class HeaderMenuComponent {
         path: route.path,
         children: route.children ? this.extractMenuItems(route.children) : null,
       }));
+
   }
 
   get getHeaderMenuItems() {
@@ -88,27 +48,6 @@ export class HeaderMenuComponent {
     return this.menuItems;
   }
 
-  fileToLoad(event: Event, type: string, headerData: any, submenuData: any = '') {
-/*
-    let urlString = headerData.url;
-
-    if(type == "subMenu")
-    {
-      urlString += "/" + submenuData.url;
-    }
-
-    event.preventDefault();
-
-    this.location.replaceState(urlString);
-
-    this.parentEvent.emit();
-*/
-  }
-
-  // Add trackById method to avoid the error
- /* trackById(index: number, item: any): string {
-    return item.id; // Track by unique `id` field
-  }*/
 }
 
 
