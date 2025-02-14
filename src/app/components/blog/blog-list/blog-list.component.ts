a6045e73cbdd1683ed44cc0816a20d4005b24939
rtlehr <itsme@rosslehr.com>
2025-02-14 13:50:08 -0500

    Added animation and view featured posts to blog

--- src/app/components/blog/blog-list/blog-list.component.ts
+++ src/app/components/blog/blog-list/blog-list.component.ts
@@ -1,6 +1,4 @@
-// blog-list.component.ts
-
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, QueryList, ViewChildren, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Location } from '@angular/common'; 
 import { ActivatedRoute } from '@angular/router';
@@ -22,7 +20,7 @@ interface pageContent {
   styleUrls: ['./blog-list.component.scss'],
   providers: [BlogService],
 })
-export class BlogListComponent implements OnInit {
+export class BlogListComponent implements OnInit, AfterViewInit {
 
   constructor(private blogService: BlogService, 
     private router: Router, 
@@ -31,29 +29,74 @@ export class BlogListComponent implements OnInit {
 
   @Input() fileToLoad = '';
 
-  pageContent: pageContent [] = []; 
-
+  pageContent: pageContent[] = []; 
   blogPosts: BlogPost[] = [];
-  
   error: string | null = null;
+  isVisible: boolean = false;
+
+  @ViewChildren('blogItem') blogDivs!: QueryList<ElementRef>;
+
+  showFeaturedOnly: boolean = false; // Flag for toggling featured posts
 
   ngOnInit(): void {
-
     this.pageContent = this.activatedRoute.snapshot.data['pageContent']; 
+    this.getAllPosts();
+  }
 
+  getAllPosts(): void {
     this.blogService.getAllPosts(this.pageContent[0].contentFile).subscribe({
-      next: (posts) => (this.blogPosts = posts),
-      error: (err) => (this.error = 'Failed to load blog posts: ' + err.message),
+      next: (posts) => {
+        this.blogPosts = posts;
+      },
+      error: (err) => {
+        this.error = 'Failed to load blog posts: ' + err.message;
+      },
+    });
+  }
+
+  getFeaturedPosts(): void {
+    this.blogService.getFeaturedPosts(this.pageContent[0].contentFile).subscribe({
+      next: (posts) => {
+        this.blogPosts = posts;
+      },
+      error: (err) => {
+        this.error = 'Failed to load blog posts: ' + err.message;
+      },
+    });
+  }
+
+  toggleFeaturedPosts(): void {
+    
+    this.blogDivs.forEach((div) => {
+      div.nativeElement.classList.remove('visible');
     });
 
+    this.showFeaturedOnly = !this.showFeaturedOnly;
+    if (this.showFeaturedOnly) {
+      this.getFeaturedPosts(); // Show featured posts only
+    } else {
+      this.getAllPosts(); // Show all posts
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.blogDivs.changes.subscribe(() => {
+      if (this.blogDivs.length === this.blogPosts.length) {
+        this.onAllPostsRendered();
+      }
+    });
+  }
+
+  onAllPostsRendered(): void {
+    this.blogDivs.forEach((div, index) => {
+      setTimeout(() => {
+        div.nativeElement.classList.add('visible');
+      }, index * 100);
+    });
   }
 
   navigateToDetails(url: string): void {
-
     const currurl = this.location.path();
-
     this.router.navigate([currurl, url]);
-
   }
-
 }
